d1417ff9129498d7f180c2047e801fe831a7618b
Varun Kumar <115029545+MauryaTech22@users.noreply.github.com>
2026-01-15 17:55:40 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+from datetime import date
+name = input("Enter your name: ")
+role = input("Enter your internship role: ")
+today_date = date.today()
+print("\nInternshiop Details")
+print("Name:" , name)
+print("Role:" , role)
+print("Date:" , today_date)
